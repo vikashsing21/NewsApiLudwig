0058ce20190b50941490dc10805a4308b42e7f70
vikash21 <vikash.infobizzs@gmail.com>
2020-02-25 14:22:33 +0530

    Pos Neg for Genreal And Health

--- rest_api.py
+++ rest_api.py
@@ -9,15 +9,16 @@ model = LudwigModel.load('./results/api_experiment_run/model')
 
 newsapi = NewsApiClient(api_key='3bff10bbdc444b09bbe7ddf8e6b381e3')
 
+
 def results(news):
-    df_data=pd.read_json(json.dumps(news))
-    df_pred=model.predict(data_df=df_data)
+    df_data = pd.read_json(json.dumps(news))
+    df_pred = model.predict(data_df=df_data)
     # combining news data with predicted result
-    df_combine=pd.concat(
+    df_combine = pd.concat(
         [df_data, df_pred['news_sentiment_predictions']], axis=1)
     # now formatting this result in proper order similar to newsapi structure
-    df_temp=pd.DataFrame(df_combine)
-    responseData=json.loads(df_temp.to_json(orient="records"))
+    df_temp = pd.DataFrame(df_combine)
+    responseData = json.loads(df_temp.to_json(orient="records"))
     return responseData
 
 
@@ -28,8 +29,9 @@ def topGen():
                                                       country='in')
     return results(top_headlines_general['articles'])
 
+
 def topEnt():
-    top_headlines_entertainment=newsapi.get_top_headlines(category='entertainment',
+    top_headlines_entertainment = newsapi.get_top_headlines(category='entertainment',
                                                             language='en',
                                                             page_size=100,
                                                             country='in')
@@ -37,7 +39,7 @@ def topEnt():
 
 
 def topTech():
-    top_headlines_technology=newsapi.get_top_headlines(category='technology',
+    top_headlines_technology = newsapi.get_top_headlines(category='technology',
                                                          language='en',
                                                          page_size=100,
                                                          country='in')
@@ -45,7 +47,7 @@ def topTech():
 
 
 def topScience():
-    top_headlines_science=newsapi.get_top_headlines(category='science',
+    top_headlines_science = newsapi.get_top_headlines(category='science',
                                                       language='en',
                                                       page_size=100,
                                                       country='in')
@@ -53,7 +55,7 @@ def topScience():
 
 
 def topSports():
-    top_headlines_sports=newsapi.get_top_headlines(category='sports',
+    top_headlines_sports = newsapi.get_top_headlines(category='sports',
                                                      language='en',
                                                      page_size=100,
                                                      country='in')
@@ -61,53 +63,77 @@ def topSports():
 
 
 def topHealth():
-    top_headlines_health=newsapi.get_top_headlines(category='health',
+    top_headlines_health = newsapi.get_top_headlines(category='health',
                                                      language='en',
                                                      page_size=100,
                                                      country='in')
     return results(top_headlines_health['articles'])
 
 
+def positivenews(newsData):
+    posdata = []
+    for data in newsData:
+        if data['news_sentiment_predictions'] == 'positive' or data['news_sentiment_predictions'] == 'neutral':
+            posdata.append(data)
+    return posdata
 
 
-@app.route('/predict/general', methods=['GET'])
-def predictGen():
-    response=topGen()
+def negativenews(newsData):
+    negdata = []
+    for data in newsData:
+        if data['news_sentiment_predictions'] == 'negative':
+            negdata.append(data)
+    return negdata
+
+# General
+@app.route('/predict/general/positive', methods=['GET'])
+def predictGenPos():
+    response = positivenews(topGen())
+    return jsonify(response)
+
+@app.route('/predict/general/negative', methods=['GET'])
+def predictGenNeg():
+    response = negativenews(topGen())
+    return jsonify(response)
+
+# Health
+@app.route('/predict/health/positive', methods=['GET'])
+def predictHealthPos():
+    response = positivenews(topHealth())
+    return jsonify(response)
+
+@app.route('/predict/health/negative', methods=['GET'])
+def predictHealthNeg():
+    response = negativenews(topHealth())
     return jsonify(response)
 
 
 @app.route('/predict/technology', methods=['GET'])
 def predictTech():
-    response=topTech()
+    response = topTech()
     return jsonify(response)
 
 
 @app.route('/predict/science', methods=['GET'])
 def predictScience():
-    response=topScience()
+    response = topScience()
     return jsonify(response)
 
 
 @app.route('/predict/entertainment', methods=['GET'])
 def predictEnt():
-    response=topEnt()
-    return jsonify(response)
-
-
-@app.route('/predict/health', methods=['GET'])
-def predictHealth():
-    response=topHealth()
+    response = topEnt()
     return jsonify(response)
 
 
 @app.route('/predict/sports', methods=['GET'])
 def predictSports():
-    response=topSports()
+    response = topSports()
     return jsonify(response)
 
 
-# if __name__ == '__main__':
-#     app.run(host='localhost', port=8000, debug=True)
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000, debug=True)
 
 #     spacy==2.2.3
 # https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.5/en_core_web_sm-2.2.5.tar.gz#egg=en_core_web_sm==2.2.5
